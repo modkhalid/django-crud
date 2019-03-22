0759daee3a5ae311c717f0ca5f51548388c479e7
modkhalid <modkhalid5@gmail.com>
2019-03-23 01:14:44 +0530

    adding static files to the project

--- level1/settings.py
+++ level1/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    STATIC_DIR
+]
--- second_app/urls.py
+++ second_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns=[
-    url(r'^a/$',views.index,name="second"),
-    url(r'^help/$',views.help,name="help")
+    url(r'^/$',views.index,name="second"),
+    url(r'^help/$',views.help,name="help"),
+    
 ]
