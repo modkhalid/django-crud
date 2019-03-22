2ad8c6040d73755393a99768530992ae4baec24b
modkhalid <modkhalid5@gmail.com>
2019-03-23 00:41:59 +0530

    include form dj.conf.urls

--- level1/settings.py
+++ level1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first_app',
+    'second_app',
 ]
 
 MIDDLEWARE = [
--- level1/urls.py
+++ level1/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from first_app import views
+from first_app import views 
+from second_app import views as v2
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.index,name="index")
+    url(r'^$',views.index,name="index"),
+    url(r'^second/',include('second_app.urls'))
 ]
