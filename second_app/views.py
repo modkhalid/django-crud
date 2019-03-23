fe2d1914d259bb47e2f661e37cd059a3d6a16245
modkhalid <modkhalid5@gmail.com>
2019-03-23 08:41:06 +0530

    displaying or fetch data of models

--- second_app/views.py
+++ second_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from second_app.models import AccessRecord
 # Create your views here.
 def index(request):
     dic={'content':"hello my name is khalid khan"}
@@ -8,3 +9,9 @@ def index(request):
 def help(request):
     dic={'help':'i cant help'}
     return render(request,"help.html",context=dic)
+
+def dater(request):
+    date_list=AccessRecord.objects.order_by('date')
+    # print(date_list[0].name)
+    dic={'my_list':date_list}
+    return render(request,'list.html',context=dic)
