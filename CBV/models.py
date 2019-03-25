3b704d1baef1287e3d8d59539f3492f2f2199161
modkhalid <modkhalid5@gmail.com>
2019-03-25 23:32:12 +0530

    adding student and schools class

--- CBV/models.py
+++ CBV/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name=models.CharField(max_length=256)
+    principal=models.CharField(max_length=256)
+    address=models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+    
+
+class Student(models.Model):
+    name=models.CharField(max_length=256)
+    school=models.ForeignKey(School,related_name='student')
+    age-models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
